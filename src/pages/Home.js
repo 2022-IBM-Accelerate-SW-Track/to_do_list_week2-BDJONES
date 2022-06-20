6ad6d37863d291e6835c18b64ef8cce42f291fb4
Brandon Jones <bdjones15@crimson.ua.edu>
2022-06-20 20:59:38 +0100

    Week 2 first submissionD

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,10 +21,24 @@ class Home extends Component {
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
     // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
+    if (this.state.todos.includes(todo)) {
+      return;
+    }
+    else {
+      let new_list = [...this.state.todos, todo];
+      // Update the local state with the new array.
+      this.setState({
+        todos: new_list,
+      });
+    }
+  };
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
     this.setState({
-      todos: new_list,
+      todos: todos,
     });
   };
   render() {
@@ -34,6 +48,7 @@ class Home extends Component {
         {/* When passing the AddTodo component, addTodo is a prop that is used in the 
         AddTodo.js file when handling the submit */}
         <AddTodo addTodo={this.addTodo} />
+        <DeleteTodo deleteTodo={this.deleteTodo}/>
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
         <Todos todos={this.state.todos} />
